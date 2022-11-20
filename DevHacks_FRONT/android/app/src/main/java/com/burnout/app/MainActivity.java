a2b9a35f9804dd5ae0ce8a3a85b12a4e929cdad1
savaiulian11 <58985829+savaiulian11@users.noreply.github.com>
2022-11-20 11:50:14 +0200

    .

--- DevHacks_FRONT/android/app/src/main/java/com/burnout/app/MainActivity.java
+++ DevHacks_FRONT/android/app/src/main/java/com/burnout/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.burnout.app;
+package com.BURNOUT.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- DevHacks_FRONT/capacitor.config.ts
+++ DevHacks_FRONT/capacitor.config.ts
@@ -2,9 +2,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.burnout.app',
+  appId: 'com.BURNOUT.app',
   appName: 'BURNOUT',
-  webDir:'dist/dev-hacks-front',
+  webDir:'dist',
   bundledWebRuntime: false
 };
 
